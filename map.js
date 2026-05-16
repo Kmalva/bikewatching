4970233e336a125e5673a6781d1e77957aeade4d
Kmalva <alvarezkatem@gmail.com>
2026-05-15 17:49:37 -0700

    Finish bikewatching visualization

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN_HERE';
+mapboxgl.accessToken = 'pk.eyJ1Ijoia21hMDA1IiwiYSI6ImNtcDdqcHVndjA0MTIycnExYmpyZjgzbXQifQ.U2tfLW_P1z-tAJSjqHBt6g';
 
 const STATIONS_URL =
   'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
